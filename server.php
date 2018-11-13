446af6c356816293b0bee77c7bd437e9c70b204c
isanchez340 <sanchez.irving006@gmail.com>
2018-11-12 18:47:01 -0800

    Add files via upload
    updated server.php

--- server.php
+++ server.php
@@ -1,12 +1,7 @@
 <?php
-   //Specify the Database server host
    define('DB_SERVER', 'ilink.cs.utep.edu');
-   //Specify the Database username
-   define('DB_USERNAME', 'cs_jagutierrez2');
-   //Specify the Database password
-   define('DB_PASSWORD', 'cs4342');
-   //Choose the Database (name)
-   define('DB_DATABASE', 'f18cs4342team8');
-   //We make the connection.
+   define('DB_USERNAME', 'cs_rxjimeneztodd');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'f18_team8');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
