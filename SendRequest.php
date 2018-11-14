5d4abf6c6292d4846ca0ecb4df8c4a83e783f46b
isanchez340 <sanchez.irving006@gmail.com>
2018-11-13 16:35:11 -0800

    Add files via upload

---
+++ SendRequest.php
@@ -0,0 +1,36 @@
+<html><body>
+	<h1>Electronic Access - Web Interface</h1>
+	 <br/>
+	 <h2>Main Menu</h2>
+
+Name: <input type="text" /><br/>
+<br/>
+Days of Access: <input type="text" /><br/>
+<br/>
+Hours of Access: <input type="text" /><br/>
+<br/>
+Start Date: <input type="text" /><br/>
+<br/>
+End Date: <input type="text" /><br/>
+<br/>
+Room Number: <input type="text" /><br/>
+<br/>
+Student ID: <input type="text" /><br/>
+<br/>
+Email: <input type="text" /><br/>
+<br/>
+Phone Number: <input type="text" /><br/>
+<br/>
+Expected Graduation Date: <input type="text" /><br/>
+<br/>
+
+
+<input type="submit" /><br/>
+</form>
+</body></html>
+
+<!--<select> 
+<option>Paint</option>
+<option>Brushes</option>
+<option>Erasers</option>
+</select>-->
