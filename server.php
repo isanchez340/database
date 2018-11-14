a135873d7f55b09d4e067442c1b809d1f732d498
isanchez340 <sanchez.irving006@gmail.com>
2018-11-13 17:23:53 -0700

    Delete server.php

--- server.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'ilink.cs.utep.edu');
-   define('DB_USERNAME', 'cs_rxjimeneztodd');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'f18_team8');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
